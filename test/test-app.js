1794c510f6116f91e08367c79b7e25146f742476
Brennan Meadowcroft <brennan@brennanmeadowcroft.com>
2015-04-29 22:57:22 -0600

    Add tests around generator

--- test/test-app.js
+++ test/test-app.js
@@ -6,19 +6,25 @@ var helpers = require('yeoman-generator').test;
 var os = require('os');
 
 describe('express-resource:app', function () {
+  var myResource = 'test';
+
   before(function (done) {
     helpers.run(path.join(__dirname, '../app'))
       .withOptions({ skipInstall: true })
-      .withPrompts({ someOption: true })
+      // .withPrompts({ resourceName: myResource,
+      //                 basePath: ,
+      //                 controllerFolder: '',
+      //                 modelFolder: '',
+      //                 testFolder: '' })
+      .withPrompts({ resourceName: myResource })
       .on('end', done);
   });
 
   it('creates files', function () {
     assert.file([
-      'bower.json',
-      'package.json',
-      '.editorconfig',
-      '.jshintrc'
+      'app/controllers/'+myResource+'.js',
+      'app/models/'+myResource+'.js',
+      'test/'+myResource+'Spec.js',
     ]);
   });
 });
